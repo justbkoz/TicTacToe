068b76d69f5feac3f49774578281b28e903ed504
Daniel Kozminski <danielkozminski@hotmail.com>
2021-05-03 15:24:29 -0400

    code split into 3 modules

--- tictactoe.js
+++ tictactoe.js
@@ -1,158 +1,175 @@
+let player = (function () {
+    const _playerFactory = (name) => {
+        this.name = name;
+        this.turn = false;
+        this.wins = 0;
+        return { name, turn, wins }
+    }
 
-const board = document.getElementById('gameBoard');
-const restart = document.getElementById('restart');
-const resetScore = document.getElementById('resetScore');
-const resetAll = document.getElementById('resetAll');
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
-const playerA = document.getElementById('playerA');
-const playerB = document.getElementById('playerB');
-const ATurn = document.getElementById('ATurn');
-const BTurn = document.getElementById('BTurn');
-const start = document.getElementById('start');
+    let a = _playerFactory('Player1');
+    let b = _playerFactory('Player2');
 
-let gBoard = new Array();
+    return { a, b }
+})();
 
-start.addEventListener('click', startGame);
-restart.addEventListener('click', restartGame);
-resetScore.addEventListener('click', resetWins);
-resetAll.addEventListener('click', resetPage);
 
-const playerFactory = (name) => {
-    this.name = name;
-    this.turn = false;
-    this.wins = 0;
-    return { name, turn, wins }
-}
 
-(function () { // Populate gBoard Array
+let gameModule = (function () {
+
+    const restart = document.getElementById('restart');
+    const resetScore = document.getElementById('resetScore');
+    const resetAll = document.getElementById('resetAll');
+    const player1 = document.getElementById('player1');
+    const player2 = document.getElementById('player2');
+    const playerA = document.getElementById('playerA');
+    const playerB = document.getElementById('playerB');
+    const ATurn = document.getElementById('ATurn');
+    const BTurn = document.getElementById('BTurn');
+    const start = document.getElementById('start');
+
+    start.addEventListener('click', startGame);
+    restart.addEventListener('click', restartGame);
+    resetScore.addEventListener('click', resetWins);
+    resetAll.addEventListener('click', resetPage);
+
+    function whoGoesFirst() {
+        if (Math.floor(Math.random() < .5)) {
+            player.a.turn = true;
+            player.b.turn = false;
+        } else {
+            player.b.turn = true;
+            player.a.turn = false;
+        }
+    };
+
+    function altTurns() {
+        player.a.turn = !player.a.turn;
+        player.b.turn = !player.b.turn;
+    }
+
+    function displayTurn() {
+        if (player.a.turn) {
+            ATurn.innerText = `X's turn`
+        } else { ATurn.innerText = '' }
+        if (player.b.turn) {
+            BTurn.innerText = `O's turn`
+        } else { BTurn.innerText = '' }
+    }
+
+    function turnFunction() {
+        if (this.innerText !== '') {
+            return
+        } else if (playerA.innerText == '') {
+            startGame();
+        }
+        else if (player.a.turn) {
+            this.innerText = 'X';
+        } else if (player.b.turn) {
+            this.innerText = 'O'
+        }
+        altTurns();
+        displayTurn();
+        winner();
+    }
+
+    function restartGame() {
+        for (i = 0; i < board.arr.length; i++) {
+            board.arr[i].innerText = '';
+        }
+    }
+
+    function playerNames() {
+        if (player1.value !== '') {
+            player.a.name = player1.value;
+        }
+        if (player2.value !== '') {
+            player.b.name = player2.value;
+        }
+    }
+
+    function startGame() {
+        playerNames();
+        playerA.innerText =
+            `${player.a.name}\n${player.a.wins} Wins`
+        playerB.innerText =
+            `${player.b.name}\n${player.b.wins} Wins`
+        start.style.display = 'none';
+        whoGoesFirst();
+        displayTurn();
+    }
+
+    function winner() {
+        let winCond = [
+            board.arr[0].innerText + board.arr[1].innerText + board.arr[2].innerText,
+            board.arr[3].innerText + board.arr[4].innerText + board.arr[5].innerText,
+            board.arr[6].innerText + board.arr[7].innerText + board.arr[8].innerText,
+            board.arr[0].innerText + board.arr[3].innerText + board.arr[6].innerText,
+            board.arr[1].innerText + board.arr[4].innerText + board.arr[7].innerText,
+            board.arr[2].innerText + board.arr[5].innerText + board.arr[8].innerText,
+            board.arr[0].innerText + board.arr[4].innerText + board.arr[8].innerText,
+            board.arr[2].innerText + board.arr[4].innerText + board.arr[6].innerText]
+
+        if (winCond.includes('XXX')) {
+            player.a.wins += 1;
+            startGame();
+            setTimeout(() => restartGame(), 1000)
+        } else if (winCond.includes('OOO')) {
+            player.b.wins += 1;
+            startGame();
+            setTimeout(() => restartGame(), 1000);
+        } else tie();
+    }
+
+    function tie() {
+        let temp = '';
+        for (i = 0; i < 9; i++) {
+            temp += board.arr[i].innerText;
+        }
+        if (temp.length == 9) {
+            BTurn.innerText = 'Tie!'
+            ATurn.innerText = 'Tie!'
+            setTimeout(() => restartGame(), 1000)
+        }
+    }
+
+    function resetWins() {
+        player.a.wins = 0;
+        player.b.wins = 0;
+        playerA.innerText =
+            `${player.a.name}\n${player.a.wins} Wins`
+        playerB.innerText =
+            `${player.b.name}\n${player.b.wins} Wins`
+    }
+
+    function resetPage() {
+        location.reload();
+    }
+
+    return {
+        turnFunction: turnFunction
+    }
+})();
+
+
+
+
+let board = (function () {
+    const board = document.getElementById('gameBoard');
+    let arr = new Array();
+
+    // Populate arr Array
     for (i = 0; i < 9; i++) {
         temp = document.createElement('div');
         temp.className = 'board';
         temp.id = `board${i}`
-        temp.addEventListener('click', turnFunction);
-        gBoard.push(temp)
+        temp.addEventListener('click', gameModule.turnFunction);
+        arr.push(temp)
     }
+
+    // Render Board from Array
+    for (i = 0; i < arr.length; i++) {
+        board.appendChild(arr[i])
+    }
+
+    return { arr }
 })();
-
-(function () { // Render Board
-    for (i = 0; i < gBoard.length; i++) {
-        board.appendChild(gBoard[i])
-    }
-})();
-
-function whoGoesFirst() {
-    if (Math.floor(Math.random() < .5)) {
-        play1.turn = true;
-        play2.turn = false;
-    } else {
-        play2.turn = true;
-        play1.turn = false;
-    }
-};
-
-function altTurns() {
-    play1.turn = !play1.turn;
-    play2.turn = !play2.turn;
-}
-
-function displayTurn() {
-    if (play1.turn) {
-        ATurn.innerText = `X's turn`
-    } else { ATurn.innerText = '' }
-    if (play2.turn) {
-        BTurn.innerText = `O's turn`
-    } else { BTurn.innerText = '' }
-}
-
-function turnFunction() {
-    if (this.innerText !== '') {
-        return
-    } else if (playerA.innerText == '') {
-        startGame();
-    }
-    else if (play1.turn) {
-        this.innerText = 'X';
-    } else if (play2.turn) {
-        this.innerText = 'O'
-    }
-    altTurns();
-    displayTurn();
-    winner();
-}
-
-function restartGame() {
-    for (i = 0; i < gBoard.length; i++) {
-        gBoard[i].innerText = '';
-    }
-}
-
-function playerNames() {
-    if (player1.value !== '') {
-        play1.name = player1.value;
-    }
-    if (player2.value !== '') {
-        play2.name = player2.value;
-    }
-}
-
-function startGame() {
-    playerNames();
-    playerA.innerText =
-        `${play1.name}\n${play1.wins} Wins`
-    playerB.innerText =
-        `${play2.name}\n${play2.wins} Wins`
-    start.style.display = 'none';
-    whoGoesFirst();
-    displayTurn();
-}
-
-function winner() {
-    let winCond = [
-        gBoard[0].innerText + gBoard[1].innerText + gBoard[2].innerText,
-        gBoard[3].innerText + gBoard[4].innerText + gBoard[5].innerText,
-        gBoard[6].innerText + gBoard[7].innerText + gBoard[8].innerText,
-        gBoard[0].innerText + gBoard[3].innerText + gBoard[6].innerText,
-        gBoard[1].innerText + gBoard[4].innerText + gBoard[7].innerText,
-        gBoard[2].innerText + gBoard[5].innerText + gBoard[8].innerText,
-        gBoard[0].innerText + gBoard[4].innerText + gBoard[8].innerText,
-        gBoard[2].innerText + gBoard[4].innerText + gBoard[6].innerText]
-
-    if (winCond.includes('XXX')) {
-        play1.wins += 1;
-        startGame();
-        setTimeout(() => restartGame(), 1000)
-    } else if (winCond.includes('OOO')) {
-        play2.wins += 1;
-        startGame();
-        setTimeout(() => restartGame(), 1000);
-    } else tie();
-}
-
-function tie() {
-    let temp = '';
-    for (i = 0; i < 9; i++) {
-        temp += gBoard[i].innerText;
-    }
-    if (temp.length == 9) {
-        BTurn.innerText = 'Tie!'
-        ATurn.innerText = 'Tie!'
-        setTimeout(() => restartGame(), 1000)
-    }
-}
-
-function resetWins() {
-    play1.wins = 0;
-    play2.wins = 0;
-    playerA.innerText =
-        `${play1.name}\n${play1.wins} Wins`
-    playerB.innerText =
-        `${play2.name}\n${play2.wins} Wins`
-}
-
-function resetPage() {
-    location.reload();
-}
-
-let play1 = playerFactory('Player1');
-let play2 = playerFactory('Player2');
